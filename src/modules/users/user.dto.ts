3d64a5680e78102a963b906882a95263335d92ac
Wagner Reis <wagnerreis1997@gmail.com>
2024-10-11 21:00:14 -0300

    feat: add hash to password

--- src/modules/users/user.dto.ts
+++ src/modules/users/user.dto.ts
@@ -2,4 +2,5 @@ export type UserCreateDTO = {
   name: string;
   email: string;
   password: string;
+  username: string;
 };
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,15 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UserCreateDTO } from './user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './users.schema';
 import { Model } from 'mongoose';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   async create(userDTO: UserCreateDTO) {
-    const userCreated = new this.userModel(userDTO);
+    const userAlreadyExists = await this.userModel
+      .findOne({
+        username: userDTO.username,
+      })
+      .exec();
+
+    if (userAlreadyExists) {
+      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+    }
+
+    const password = await hash(userDTO.password, 10);
+
+    const userCreated = new this.userModel({ ...userDTO, password });
     const user = await userCreated.save();
     console.log(user);
     return user;
